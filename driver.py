f0a43b9438211fc761878ac3f798100d696f4049
Miguel Torres <migueltorres@Miguels-MacBook-Pro.local>
2024-12-09 21:36:20 +0100

    subo codigo

---
+++ driver.py
@@ -0,0 +1,98 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.firefox.options import Options
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+
+import time
+
+import numpy as np
+
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+import re
+
+class Driver: 
+    def __init__(self, options=Options()):
+        self.driver = webdriver.Chrome(options=options)
+
+    def busca_cr(self, fecha_ini, fecha_fin, hora_buscada, pista_buscada, duracion_buscada):        
+        # Construyo URL
+        url = f"https://playtomic.io/ciudad-de-la-raqueta/da78dd3c-43b3-11e8-8674-52540049669c?q=TENNIS~{fecha_ini.year}-{fecha_ini.month}-{fecha_ini.day}~~~"
+        self.driver.get(url)
+        self.driver.maximize_window()
+
+        self.driver.implicitly_wait(5)
+        nombre_pistas = self.driver.find_elements(by=By.XPATH, value="//div[@class='bbq2__resource__label']")
+
+        filas_slots = self.driver.find_elements(by=By.XPATH, value="//div[@class='bbq2__slots-resource']")
+        for i in range(len(filas_slots)): 
+            # print(f"======\nCompruebo slots de la pista {nombre_pistas[i].text}")
+
+            slots = filas_slots[i].find_elements(by=By.XPATH, value=".//div[@class='bbq2__slot']/div")
+            # print(f"He encontrado {len(slots)} slots vacíos")
+
+            for j in slots: 
+                self.driver.implicitly_wait(5)
+                self.driver.execute_script("arguments[0].click();", j)
+
+                hora = self.driver.find_element(by=By.CLASS_NAME, value="bbq2__duration-picker__time").text
+
+                duraciones_block = self.driver.find_elements(by=By.CLASS_NAME, value="bbq2__duration-picker__option")
+                duraciones_posibles = []
+                for duracion in duraciones_block:
+                    hora_formateada = duracion.find_element(by=By.XPATH, value=".//div").text
+                    duraciones_posibles.append(self.obtener_duracion(hora_formateada))
+                    
+                # print(f"He encontrado hueco en la pista {nombre_pistas[i].text} a las {hora} con duraciones posibles de {duraciones_posibles}")
+
+                modalidad = "descubierto" if ("descubierto") in nombre_pistas[i].text.lower() else "cubierto"
+                if modalidad == pista_buscada.lower() and hora == hora_buscada and duracion_buscada in duraciones_posibles: 
+                    dia_semana = self.obtener_dia_semana(fecha_ini.weekday())
+                    print(f"La hora más cercana a tu modalidad de pista es el {dia_semana} {fecha_ini.day}/{fecha_ini.month}/{fecha_ini.year} a las {hora} en la pista {nombre_pistas[i].text}. Las duraciones posibles son de {duraciones_posibles} horas")
+
+                    # Compro pista
+                    btn_comprar = self.driver.find_element(by=By.CLASS_NAME, value="bbq2__duration-picker__button")
+                    self.driver.execute_script("arguments[0].click();", btn_comprar)
+
+                    cuadro_usuario = self.driver.find_element(by=By.ID, value="sign-up__email")
+                    cuadro_contra = self.driver.find_element(by=By.ID, value="sign-up__password")
+
+                    cuadro_usuario.send_keys("mtorresvalls1@gmail.com")
+                    cuadro_contra.send_keys("Miguel270302")
+
+                    self.driver.find_element(by=By.ID, value="sign-in__submit").click()
+                    time.sleep(10)
+                    return 0
+
+                # Cierro slot
+                btn_cerrar = self.driver.find_element(by=By.CLASS_NAME, value="bbq2__duration-picker__close")
+                self.driver.execute_script("arguments[0].click();", btn_cerrar)
+        
+        if fecha_ini <= fecha_fin: 
+            self.busca_cr(fecha_ini + relativedelta(days=1), fecha_fin, hora_buscada, pista_buscada, duracion_buscada)
+        return 1
+
+    def obtener_dia_semana(self, codigo):
+        dias_semana = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"]
+
+        return dias_semana[codigo]
+
+    def obtener_duracion(self, texto):
+        # Buscar el patrón de horas y minutos
+        patron = r"(\d+)\s*h\s*(\d+)?\s*min"
+        match = re.search(patron, texto)
+        
+        horas = int(match.group(1))  
+        minutos = int(match.group(2)) if match.group(2) else 0  
+        duracion = horas + minutos / 60
+        
+        return duracion
+     
+    def close(self):
+        self.driver.quit()
---
+++ main.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+from selenium.webdriver.chrome.options import Options
+
+import time
+
+from driver import Driver
+
+def main(): 
+    # Cargo driver de chrome
+    options = Options()
+    # options.add_argument("--window-size=1920,1080")
+    # options.add_argument("--headless=new")
+    # options.add_argument("--disable-gpu")
+    # options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
+
+    driver = Driver(options)
+    print("Driver de Chrome lanzado correctamente")
+
+    fecha_ini = datetime.now() + relativedelta(days=1)
+    fecha_fin = fecha_ini + relativedelta(days=6)
+
+    hora_buscada = "10:00"
+    pista_buscada = "cubierto"
+    duracion_buscada = 2
+
+    driver.busca_cr(fecha_ini, fecha_fin, hora_buscada, pista_buscada, duracion_buscada)
+
+    # time.sleep(5)
+
+    driver.close()
+
+if __name__ == "__main__":
+    main()
